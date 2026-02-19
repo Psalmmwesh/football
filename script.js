64e7a63972615f2e17a89c3045151eed19252fe4
Psalmmwesh <162792276+Psalmmwesh@users.noreply.github.com>
2026-02-19 18:09:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+    // close menu
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+());
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
